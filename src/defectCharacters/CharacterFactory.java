a4f4b8109704a5e7af3803d454e5cd1b0b5efc97
BIRGER <birger.lehmann>
2025-08-04 18:30:58 +0200

    no change to test

--- src/defectCharacters/CharacterFactory.java
+++ src/defectCharacters/CharacterFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class CharacterFactory {
+public class CharacterFactory { 
 
 	public static CharacterFactory getInstance() {
 		return new CharacterFactory();
